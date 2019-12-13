8bb1c76e0785600ea2719fa109354f9737145759
mariumtapal <mtapal@smith.edu>
2019-12-13 17:00:14 -0500

    package documentation

---
+++ R/fec16-package.R
@@ -0,0 +1,24 @@
+#' fec16: A Package That Contains FEC data for the U.S. 2015-2016 Election Cycle
+#'
+#' The fec16 package provides Federal Election Commission data of:
+#' candidates, committees, results, individual contributions, and committee contribution.
+#'
+#' @section fec16 Datasets:
+#' \itemize{
+#'   \item Candidates: [candidates]
+#'   \item Committees: [committees]
+#'   \item Results: [results]
+#'   \item Individual Contributions: [individuals]
+#'   \item Committee Contributions: [committee_contributions]
+#' }
+#'
+#' @section Authors:
+#' \itemize{
+#'   \item Irene Ryan: [https://github.com/ireneryan]
+#'   \item Marium Tapal: [https://github.com/mariumtapal]
+#'   \item Rana Gahwagy: [https://github.com/ranawg]
+#' }
+#'
+#' @docType package
+#' @name fec16
+NULL
